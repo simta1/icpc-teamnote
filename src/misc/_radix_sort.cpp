3749c9212cb63d358c62b78893f80ac2be7aba3a
simta1 <koreajunho0219@gmail.com>
2026-02-26 02:32:35 +0900

    refactor: radix sort

--- src/misc/_radix_sort.cpp
+++ src/misc/_radix_sort.cpp
@@ -1,9 +1,6 @@
-[카테고리](/README.md)
-## Radix Sort
-```cpp
 template <bool onlyPositive, typename T>
-void radixSort(vector<T> &v) {
-    static_assert(is_integral<T>::value, "T가 정수형 자료형이어야 함");
+void radixSort(vector<T> &v) { // int O(4N), long long O(8N)
+    static_assert(is_integral<T>::value, "T must be integer");
 
     const unsigned int SZ = 8;
     const unsigned int mask = (1 << SZ) - 1;
@@ -28,19 +25,3 @@ void radixSort(vector<T> &v) {
         }
     }
 }
-```
-### 시간복잡도 
-int $O(4N)$   
-long long $O(8N)$   
-
-### 사용설명
-정수형에서만 사용   
-```cpp
-vector<int> v(n);
-for (auto &e : v) cin >> e;
-radixSort<true>(v); // 양수만 주어질 때
-radixSort<false>(v); // 음수도 주어질 때
-```
-
-### 문제
-[수 정렬하기 2](https://www.acmicpc.net/problem/2751)
