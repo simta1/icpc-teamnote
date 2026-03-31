b46a66680a0d6520481d22d4feac0d768eac0b9b
simta1 <koreajunho0219@gmail.com>
2026-04-01 01:22:13 +0900

    fix: small to large

--- src/data_structure/_small_to_large.cpp
+++ src/data_structure/_small_to_large.cpp
@@ -1,11 +1,11 @@
-// 집합을 전부 합치기까지 원소 이동 횟수는 총 O(N logN)
+// 집합을 전부 합치기까지 총 원소 이동 횟수 O(N logN)
 a = dsu.find(a);
 b = dsu.find(b);
 if (v[a].size() > v[b].size()) {
-    for (auto [l, r] : v[b].s) v[a].emplace(l, r);
+    v[a].insert(v[a].end(), v[b].begin(), v[b].end());
     dsu.merge(b, a); // parent[find(b)] = find(a);
 }
 else {
-    for (auto [l, r] : v[a].s) v[b].emplace(l, r);
+    v[b].insert(v[b].end(), v[a].begin(), v[a].end());
     dsu.merge(a, b); // parent[find(a)] = find(b);
 }
